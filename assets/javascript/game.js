78f9e0a03b6b323afb123ef548e8904bce8d2b2e
Jesse W. Salmon <j_aw_s@icloud.com>
2018-08-13 02:00:31 -0700

    add code to pick random word from words array and adjust display for new word and start of game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,9 +1,23 @@
 var words = ["England", "Scottland", "Ireland","Australia","Brazil","Belgium","Canada","Fiji","France","Israel",
              "Italy","Japan","Mexico","Portugal","Spain","Tonga"];
 var guessedLetters = [];
-var randomWord = words[Math.floor(Math.random() * words.length)];
+var currentWord = [];
+var randomWord = "";
 document.onkeyup = function (event) {
 
     // Determines which key was pressed.
     var userGuess = event.key;
+    if (/[a-zA-Z]/.test(userGuess)){
+
+    }
+    else{
+        randomWord = words[Math.floor(Math.random() * words.length)];
+        for(var i = 0; i < randomWord.length; i++){
+            currentWord.push("_ ");
+        }
+        document.getElementById("curWord").textContent = currentWord;
+        currentWord="";
+        document.getElementById("instructions").textContent = "Press any letter to continue Guessing"
+
+    }
 }
